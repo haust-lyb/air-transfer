0b7fe10053f3ce20db17f6070ade3daf906961f9
liyibo <1570194845@qq.com>
2021-01-20 18:30:29 +0800

    ...

--- test/vuedemo/vue-electron-js/src/App.vue
+++ test/vuedemo/vue-electron-js/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="item in clipboardlist">
+      {{item}}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const {ipcRenderer} = require('electron')
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      clipboardlist:[]
+    }
+  },
+  mounted() {
+    ipcRenderer.on("refreshclipboardlist",()=>{
+
+    })
   }
 }
 </script>
--- test/vuedemo/vue-electron-js/src/main.js
+++ test/vuedemo/vue-electron-js/src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+//  主进程
+import { ipcMain } from 'electron'
+new BrowserWindow({
+  parent:win,
+  width:300,
+  height:400
+})
+ipcMain.on('refreshclipboardlist',(event,param)=>{
+  //  event 是渲染进程中的webContent对象实例
+  console.log(param)
+})
 
 Vue.config.productionTip = false
 
